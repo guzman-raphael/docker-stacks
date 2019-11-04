329c287dc5ca290472e5b25bf75714a8f81d9853
guzman-raphael <raphael.h.guzman@gmail.com>
2019-11-04 12:52:16 -0600

    Update post-start to install from requirements.txt + only maintain subset of cloned repo.

--- datajoint-notebook/post-start.sh
+++ datajoint-notebook/post-start.sh
@@ -12,11 +12,28 @@
 # export HOME=/home/${JUPYTERHUB_USER}
 # export NB_USER=$JUPYTERHUB_USER
 
+# remove files and hidden files
 rm -R /home/shared/*
+rm -rf /home/shared/.* 2> /dev/null
+#clone
 git clone $NB_REPO /home/shared
+#copy global config
 cp /usr/local/bin/.datajoint_config.json ./
-
+#pip install requirements in root + pipeline
+pip install --user -r /home/shared/requirements.txt
 pip install --user /home/shared
+#copy subset
+mkdir /tmp/shared
+cp -R /home/shared/${NB_REPO_RELPATH}/* /tmp/shared
+cp -r /home/shared/${NB_REPO_RELPATH}/.[^.]* /tmp/shared
+#remove files and hidden files
+rm -R /home/shared/*
+rm -rf /home/shared/.* 2> /dev/null
+#move contents
+mv /tmp/shared/* /home/shared
+mv /tmp/shared/.[^.]* /home/shared
+#remove prev temp directory
+rm -R /tmp/shared
 
 cd /home/${NB_USER}
 
